c0f546f757569e9c77f656ec917012a4c5ecb273
ksdyb.park <ksdyb@amazon.com>
2023-02-21 12:38:56 +0900

    update

---
+++ cdk-emotion-garden/lib/cdk-emotion-garden-stack.ts
@@ -0,0 +1,139 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as path from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+
+export class CdkEmotionGardenStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const stage = "dev"; 
+    const endpoint = "jumpstart-example-infer-model-txt2img-s-2023-02-10-11-24-04-069";
+
+    // s3 
+    const s3Bucket = new s3.Bucket(this, "gg-depolyment-storage",{
+      // bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: s3Bucket.bucketName,
+      description: 'The nmae of bucket',
+    });
+    new cdk.CfnOutput(this, 's3Arn', {
+      value: s3Bucket.bucketArn,
+      description: 'The arn of s3',
+    });
+    new cdk.CfnOutput(this, 's3Path', {
+      value: 's3://'+s3Bucket.bucketName,
+      description: 'The path of s3',
+    });
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, 'cloudfront', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, 'distributionDomainName', {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    }); 
+
+    // Lambda for stable diffusion 
+    const mlLambda = new lambda.Function(this, 'lambda-stable-diffusion', {
+      description: 'lambda for stable diffusion',
+      functionName: 'lambda-stable-diffusion',
+      handler: 'lambda_function.lambda_handler',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda')),
+      timeout: cdk.Duration.seconds(60),
+      environment: {
+        bucket: s3Bucket.bucketName,
+        endpoint: endpoint,
+        domain: distribution.domainName
+      }
+    });     
+
+    s3Bucket.grantReadWrite(mlLambda); // permission for s3
+    const SageMakerPolicy = new iam.PolicyStatement({  // policy statement for sagemaker
+      actions: ['sagemaker:*'],
+      resources: ['*'],
+    });    
+    mlLambda.role?.attachInlinePolicy( // add sagemaker policy
+      new iam.Policy(this, 'sagemaker-policy', {
+        statements: [SageMakerPolicy],
+      }),
+    );
+    // version
+    const version = mlLambda.currentVersion;
+    const alias = new lambda.Alias(this, 'LambdaAlias', {
+      aliasName: stage,
+      version,
+    }); 
+    // permission for api Gateway
+    mlLambda.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+    
+    // role
+    const role = new iam.Role(this, "api-role", {
+      roleName: "ApiRole",
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+    
+    // API Gateway
+    const api = new apiGateway.RestApi(this, 'api-stable-diffusion', {
+      description: 'API Gateway',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      deployOptions: {
+        stageName: stage,
+      },
+    });  
+
+    // POST method
+    const text2image = api.root.addResource('text2image');
+    text2image.addMethod('POST', new apiGateway.LambdaIntegration(mlLambda, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    new cdk.CfnOutput(this, 'apiUrl', {
+      value: api.url,
+      description: 'The url of API Gateway',
+    }); 
+    new cdk.CfnOutput(this, 'curlUrl', {
+      value: "curl -X POST "+api.url+'text2image -H "Content-Type: application/json" -d \'{"text":"astronaut on a horse"}\'',
+      description: 'Curl commend of API Gateway',
+    }); 
+  }
+}
---
+++ lambda/lambda_function.py
@@ -0,0 +1,65 @@
+import json
+import boto3
+import io
+import os
+import time
+import base64
+
+s3 = boto3.client('s3')
+
+def parse_response(query_response):    
+    response_dict = json.loads(query_response)
+    return response_dict["generated_images"], response_dict["prompt"]
+    
+def lambda_handler(event, context):
+    print(event)
+
+    # txt = "astronaut on a horse",        
+    txt = event['text']
+    print("text: ", txt)
+
+    endpoint = os.environ.get('endpoint')
+    print("endpoint: ", endpoint)
+
+    mybucket = os.environ.get('bucket')
+    print("bucket: ", mybucket)
+
+    mykey = 'img_'+time.strftime("%Y%m%d-%H%M%S")+'.jpeg'  
+    print('key: ', mykey)
+
+    domain = os.environ.get('domain')  
+    url = "https://"+domain+'/'+mykey
+    print("url: ", url)
+            
+    payload = {        
+        "prompt": txt,
+        "width": 768,
+        "height": 512,
+        "num_images_per_prompt": 1,
+        "num_inference_steps": 50,
+        "guidance_scale": 7.5,
+    }
+
+    runtime = boto3.Session().client('sagemaker-runtime')
+    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Accept='application/json;jpeg', Body=json.dumps(payload))
+    
+    statusCode = response['ResponseMetadata']['HTTPStatusCode']
+    print('statusCode:', json.dumps(statusCode))
+    
+    if(statusCode==200):
+        response_payload = response['Body'].read().decode('utf-8')
+        generated_images, prompt = parse_response(response_payload)
+
+        #print(response_payload)
+        #print(generated_images[0])
+        print(prompt)
+        
+        img_str = base64.b64decode(generated_images[0])
+        buffer = io.BytesIO(img_str) 
+
+        s3.upload_fileobj(buffer, mybucket, mykey, ExtraArgs={"ContentType": "image/jpeg"})
+                    
+    return {
+        'statusCode': statusCode,
+        'body': url
+    }
