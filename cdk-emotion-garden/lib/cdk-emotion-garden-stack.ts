cd21c4629adb97a15a6da2ca7668e082ea87e6f8
ksdyb.park <ksdyb@amazon.com>
2023-03-14 20:14:10 +0900

    update

--- cdk-emotion-garden/lib/cdk-emotion-garden-stack.ts
+++ cdk-emotion-garden/lib/cdk-emotion-garden-stack.ts
@@ -28,7 +28,7 @@ export class CdkEmotionGardenStack extends cdk.Stack {
 
     // SQS - Bulk
     const queueBulk = new sqs.Queue(this, 'QueueBulk', {
-      visibilityTimeout: cdk.Duration.seconds(30),
+      visibilityTimeout: cdk.Duration.seconds(10),
       queueName: "queue-emotion-garden.fifo",
       fifo: true,
       contentBasedDeduplication: true,
