79091c1642cc36af2ab17461e782c32c2d1b7961
ksdyb.park <ksdyb@amazon.com>
2023-03-17 20:23:09 +0900

    update

--- lambda-s3-event/index.js
+++ lambda-s3-event/index.js
@@ -8,9 +8,25 @@ exports.handler = async (event, context) => {
     console.log('## ENVIRONMENT VARIABLES: ' + JSON.stringify(process.env));
     console.log('## EVENT: ' + JSON.stringify(event))
     
-    response = {
-        statusCode: 200,
+    // Get the object from the event and show its content type
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
     }; 
-    
+    console.log('params: ' + JSON.stringify(params))
+
+    /*try {
+        const { ContentType } = await s3.getObject(params).promise();
+        console.log('CONTENT TYPE:', ContentType);
+    } catch (err) {
+        console.log(err);
+    }*/
+
+    const response = {
+        statusCode: 200,
+    };
+
     return response;
 };
