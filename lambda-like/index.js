5b975c39170c9af0164bd9280ff086504916d072
ksdyb.park <ksdyb@amazon.com>
2023-03-23 01:34:14 +0900

    update

--- lambda-like/index.js
+++ lambda-like/index.js
@@ -30,7 +30,7 @@ exports.handler = async (event, context) => {
     }; */
     const response = {
         statusCode: 200,
-        // body: JSON.stringify(urlList)
+        body: "Success"
     };
     return response;
 };
