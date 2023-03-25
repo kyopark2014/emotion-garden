3e7c1c73740561c2437ffee94463b21b587aa88d
ksdyb.park <ksdyb@amazon.com>
2023-03-25 18:13:51 +0900

    update

--- cdk-emotion-garden/lib/cdk-emotion-garden-stack.ts
+++ cdk-emotion-garden/lib/cdk-emotion-garden-stack.ts
@@ -531,10 +531,49 @@ export class CdkEmotionGardenStack extends cdk.Stack {
     });
 
     // personalize
-    const cfnDatasetGroup = new personalize.CfnDatasetGroup(this, 'MyCfnDatasetGroup', {
+    const datasetGroup = new personalize.CfnDatasetGroup(this, 'DatasetGroup', {
       name: 'emotion-garden-dataset',
     });
-    
+
+    const interactionSchemaJson = `{
+      "type": "record",
+      "name": "Interactions",
+      "namespace": "com.amazonaws.personalize.schema",
+      "fields": [
+        {
+          "name": "USER_ID",
+          "type": "string"
+        },
+        {
+          "name": "ITEM_ID",
+          "type": "string"
+        },
+        {
+          "name": "TIMESTAMP",
+          "type": "long"
+        },
+        { 
+          "name": "EVENT_TYPE",
+          "type": "string"
+        },
+        {
+          "name": "IMPRESSION",
+          "type": "string"
+        }
+      ],
+      "version": "1.0"
+    }`;
+    const interactionSchema = new personalize.CfnSchema(this, 'InteractionSchema', {
+      name: 'interaction-schema',
+      schema: interactionSchemaJson,
+    });
+
+    const interactionDataset = new personalize.CfnDataset(this, 'InteractionDataset', {
+      datasetGroupArn: datasetGroup.attrDatasetGroupArn,
+      datasetType: 'Interactions',
+      name: 'interaction-dataset',
+      schemaArn: interactionSchema.attrSchemaArn,    
+    });
   }
 }
 
