616e38edd300986cad9bb709e924026f546878b1
ksdyb.park <ksdyb@amazon.com>
2023-04-01 17:28:40 +0900

    update

--- lambda-generate-csv/index.js
+++ lambda-generate-csv/index.js
@@ -1,9 +1,9 @@
 const aws = require('aws-sdk');
 const dynamo = new aws.DynamoDB.DocumentClient();
-const tableName = process.env.tableName;
 
 import { parse } from "json2csv";
 
+const bucketName = process.env.bucketName;
 const userTableName = process.env.userTableName;
 const interactionTableName = process.env.interactionTableName;
 const itemTableName = process.env.itemTableName;
@@ -24,38 +24,23 @@ exports.handler = async (event, context) => {
     try {
         dynamoScan = await dynamo.scan(params).promise();
 
-        console.log('queryDynamo: '+JSON.stringify(dynamoScan));
+        console.log('queryDynamo: '+JSON.stringify(dynamoScan[0]));
         console.log('queryDynamo: '+dynamoScan.Count);      
     } catch (error) {
         console.log(error);
         return;
     }  
 
-    /* 
-    var params = {
-    TableName: "NOTIFICATION",
-    FilterExpression: "user_id = :user_id and category = :category",
-    ExpressionAttributeValues: {
-        ":user_id": user_id,
-        ":category": category
-    }
-}
-
-docClient.scan(params).promise()
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    });*/
-
-/*    const payload = [{ a: 1, b: 2 }]
-    const csvPayload = parse(payload, { header: true, defaultValue: "-----"});
+    const payload = dynamoScan;
+    const csvPayload = parse(payload, { 
+        header: true, 
+        defaultValue: "-----"
+    });
 
     const s3Key = 'filename.csv';
-    const bucketName = 'bucket-name';
+    const bucketName = bucketName;
 
-    await s3.put(bucketName, s3Key, csvPayload); */
+    await s3.put(bucketName, s3Key, csvPayload); 
 
     const response = {
         statusCode: 200,
