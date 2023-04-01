547f011bfe0f62b6168b6bd188f1081cfddffdd9
ksdyb.park <ksdyb@amazon.com>
2023-04-01 18:20:24 +0900

    update

--- lambda-generate-csv/index.js
+++ lambda-generate-csv/index.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
 const dynamo = new aws.DynamoDB.DocumentClient();
-
-import { parse } from "json2csv";
+const s3 = new aws.S3();
+const json2csv = require('json2csv');
 
 const bucketName = process.env.bucketName;
 const userTableName = process.env.userTableName;
@@ -12,7 +12,10 @@ exports.handler = async (event, context) => {
     console.log('## ENVIRONMENT VARIABLES: ' + JSON.stringify(process.env));
     console.log('## EVENT: ' + JSON.stringify(event));
 
-    let params = {
+    let isCompleted = false;
+
+    // interaction
+    let interactionDynamoParams = {
         TableName: interactionTableName,
         //FilterExpression: "Emotion = :emotion",
         //ExpressionAttributeValues: {
@@ -20,27 +23,124 @@ exports.handler = async (event, context) => {
         //}
     };
 
-    let dynamoScan; 
+    let interactionDynamoScan; 
     try {
-        dynamoScan = await dynamo.scan(params).promise();
+        interactionDynamoScan = await dynamo.scan(interactionDynamoParams).promise();
 
-        console.log('queryDynamo: '+JSON.stringify(dynamoScan[0]));
-        console.log('queryDynamo: '+dynamoScan.Count);      
+        console.log('interactionDynamoScan: '+JSON.stringify(interactionDynamoScan));
+        console.log('interactionDynamoScan: '+interactionDynamoScan.Count);      
     } catch (error) {
         console.log(error);
         return;
     }  
+    
+    try {
+        const payload = interactionDynamoScan['Items'];
+        console.log('payload: ' + JSON.stringify(payload)); 
 
-    const payload = dynamoScan;
-    const csvPayload = parse(payload, { 
-        header: true, 
-        defaultValue: "-----"
-    });
+        const csvPayload = json2csv.parse(payload, { 
+            header: true, 
+        });
 
-    const s3Key = 'filename.csv';
-    const bucketName = bucketName;
+        var params = {
+            Bucket: bucketName,
+            Key: 'dataset/interactions/interactions.csv',
+            Body: csvPayload,
+        }
 
-    await s3.put(bucketName, s3Key, csvPayload); 
+        await s3.upload(params).promise();  
+    } catch (error) {
+        console.log(error);
+        return;        
+    }  
+
+    // user
+    let userDynamoParams = {
+        TableName: userTableName,
+    };
+
+    let userDynamoScan; 
+    try {
+        userDynamoScan = await dynamo.scan(userDynamoParams).promise();
+
+        console.log('userDynamoScan: '+JSON.stringify(userDynamoScan));
+        console.log('userDynamoScan: '+userDynamoScan.Count);      
+    } catch (error) {
+        console.log(error);
+        return;
+    }  
+    
+    try {
+        const payload = userDynamoScan['Items'];
+        console.log('payload: ' + JSON.stringify(payload)); 
+
+        const csvPayload = json2csv.parse(payload, { 
+            header: true, 
+        });
+
+        var params = {
+            Bucket: bucketName,
+            Key: 'dataset/users/users.csv',
+            Body: csvPayload,
+        }
+
+        await s3.upload(params).promise();  
+    } catch (error) {
+        console.log(error);
+        return;        
+    }
+    
+    // items
+    let itemDynamoParams = {
+        TableName: itemTableName,
+    };
+
+    let itemDynamoScan; 
+    try {
+        itemDynamoScan = await dynamo.scan(itemDynamoParams).promise();
+
+        console.log('itemDynamoScan: '+JSON.stringify(itemDynamoScan));
+        console.log('itemDynamoScan: '+itemDynamoScan.Count);      
+    } catch (error) {
+        console.log(error);
+        return;
+    }  
+    
+    try {
+        const payload = itemDynamoScan['Items'];
+        console.log('payload: ' + JSON.stringify(payload)); 
+
+        const csvPayload = json2csv.parse(payload, { 
+            header: true, 
+        });
+
+        var params = {
+            Bucket: bucketName,
+            Key: 'dataset/itemss/itemss.csv',
+            Body: csvPayload,
+        }
+
+        await s3.upload(params).promise();  
+    } catch (error) {
+        console.log(error);
+        return;        
+    }
+
+    function wait() {
+        return new Promise((resolve, reject) => {
+            if (!isCompleted) {
+                setTimeout(() => resolve("wait..."), 1000);
+            }
+            else {
+                setTimeout(() => resolve("done..."), 0);
+            }
+        });
+    }
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
+    console.log(await wait());
 
     const response = {
         statusCode: 200,
