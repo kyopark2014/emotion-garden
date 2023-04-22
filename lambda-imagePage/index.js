f819e93541914c390dba7da61eade34647ec155f
ksdyb.park <ksdyb@amazon.com>
2023-04-22 10:09:02 +0900

    update

--- lambda-imagePage/index.js
+++ lambda-imagePage/index.js
@@ -9,8 +9,8 @@ exports.handler = async (event, context) => {
     let contentName = event['queryStringParameters'].content;
     console.log('content: ' + contentName);
 
-    let url = 'https://'+domainName+'/'+contentName;
-    let html = `<html><body><h2>AWS Seoul Seummit: Emotion Gardens</h2><img src=`+url+`><p>`+msg+`</p></body></html>`;
+    let url = `https://${domainName}/${contentName}`;
+    let html = `<html><body><meta charset="UTF-8"><h2>AWS Seoul Seummit: Emotion Gardens</h2><img src=`+url+`><p>`+msg+`</p></body></html>`;
     console.log('html: ' + html);
 
     let response = {
